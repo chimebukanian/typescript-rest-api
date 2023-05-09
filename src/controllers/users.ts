8992396812c8b65d7d0aa975890d547b09842e86
chimebukanian <chimebukaanyanwu@gmail.com>
2023-05-09 14:27:34 +0100

    test

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { getUsers, deleteUserById, updateUserById, getUserById } from '../db/users';
 
-export const getAllUsers =async (req:express.Request, res:express.Response) => {
+export const getAllUsers = async (req:express.Request, res:express.Response) => {
     try{
         const users = await getUsers();
         return res.status(200).json(users).end();
